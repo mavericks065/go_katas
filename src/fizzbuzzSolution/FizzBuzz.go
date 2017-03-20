3ab1f140d33a35d61aa3a29286ce38d00c34ca1f
mavericks065 <nicolas065@gmail.com>
2017-03-20 14:31:38 +1100

    Basic bowling game done in TDD respecting Uncle Bob’s way

--- src/fizzbuzzSolution/FizzBuzz.go
+++ src/fizzbuzzSolution/FizzBuzz.go
@@ -2,20 +2,23 @@ package fizzbuzzSolution
 
 import "strconv"
 
+// constants to check if the input is modulo 3,5 or 15.
 const (
 	FIZZ     = 3
 	BUZZ     = 5
 	FIZZBUZZ = 15
 )
 
+// GetResult should return fizz, buzz or fizzbuzz depending
+// on if the input is modulo 3, 5 or 15.
 func GetResult(input int) string {
-	if input % FIZZBUZZ == 0 {
+	if input%FIZZBUZZ == 0 {
 		return "fizzbuzz"
 	}
-	if input % FIZZ == 0 {
+	if input%FIZZ == 0 {
 		return "fizz"
 	}
-	if input % BUZZ == 0 {
+	if input%BUZZ == 0 {
 		return "buzz"
 	}
 	return strconv.Itoa(input)
